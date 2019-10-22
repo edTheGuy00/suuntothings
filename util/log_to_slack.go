0172bd6126c62c227b5d32793ee59c3da1bde660
Edgar <edgrr.trem000@gmail.com>
2019-10-22 15:15:02 +0700

    add slack logger

---
+++ util/log_to_slack.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/gobuffalo/envy"
+)
+
+// LogToSlack sends logs to slack channel
+func LogToSlack(content string) {
+	_, err := http.PostForm("https://slack.com/api/files.upload", url.Values{
+		"token":           {envy.Get("SLACK_TOKEN", "")},
+		"channels":        {envy.Get("SLACK_LOG_CHANNEL", "")},
+		"content":         {content},
+		"filename":        {"fitbit.json"},
+		"filetype":        {"json"},
+		"initial_comment": {"test"},
+	})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
